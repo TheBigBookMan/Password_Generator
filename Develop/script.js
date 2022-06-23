cb128373830b8fe1c7451ddcf10072434e655119
Ben Smerd <bsmerd@adam.com.au>
2022-06-23 13:54:48 +0930

    Creating the window.confirm to see if the user wants to create a password and inserting the first prompt asking for length of password.

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  
 }
+
+function generatePassword() {
+  //asking user if they want to begin and with criteria//
+  var confirm = window.confirm("The password will contain a series of numbers that you get to select as well as the length, are you ready to begin?");
+
+  //if the user wants to begin or not creating the password criteria//
+  if (confirm) {
+    var firstPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?")
+  } else {
+    window.alert("Cheerio.")
+    return;
+  }
+
+
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
