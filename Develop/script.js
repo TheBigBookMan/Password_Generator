346e12ba2a9dc7f7aa938954953d2d98424a1d5f
Ben Smerd <bsmerd@adam.com.au>
2022-06-23 16:12:21 +0930

    Added in a function to generatePassword and then another function for length and conditional loop for length user input response.

--- Develop/script.js
+++ Develop/script.js
@@ -10,16 +10,31 @@ function writePassword() {
 }
 
 function generatePassword() {
+
   //asking user if they want to begin and with criteria//
   var confirm = window.confirm("The password will contain a series of numbers that you get to select as well as the length, are you ready to begin?");
 
-  //if the user wants to begin or not creating the password criteria//
-  if (confirm) {
-    var firstPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?")
+  let lengthPrompt;
+  //if the user wants to begin or not creating the password criteria with function//
+  //if they confirm they want to being then first prompt asking about length of characters//
+  var firstPrompt = function() {
+    if (confirm) {
+    lengthPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?");
+    return lengthPrompt;
   } else {
     window.alert("Cheerio.")
     return;
   }
+}
+  
+  firstPrompt()
+
+  if (lengthPrompt < 8 || lengthPrompt > 128) {
+    window.alert("That wasn't between 8 and 128 characters, please try again.")
+    firstPrompt()
+  }
+
+
 
 
 } 
