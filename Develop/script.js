9fa9503f3880d6a3b001e3de008e5ef8d4dbfc4e
Ben Smerd <bsmerd@adam.com.au>
2022-06-24 19:07:18 +0930

    Added in a goodbye statement if the user chooses to leave when being asked about length.

--- Develop/script.js
+++ Develop/script.js
@@ -38,22 +38,34 @@ function generatePassword() {
 };
 
 //if the user wants to begin or not creating the password criteria with function//
-//if they confirm they want to being then first prompt asking about length of characters//
+//if they confirm they want to begin then first prompt asking about length of characters//
   var firstPrompt = function() {
+
 //Prompt window asking for how many characters between 8 and 128
     lengthPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?");
 
-//If the user inputs number less than 8 or more than 128 then an alert to try again appears
-    if (lengthPrompt < 8 || lengthPrompt > 128) {
-      window.alert("That wasn't between 8 and 128 characters, please try again.")
-      firstPrompt()
-    } 
+//This checks if the user types in anything or presses cancel to leave
+    if (lengthPrompt) {
+
+//If the user did input numbers then the if/else statement checks the numbers
+//If the user inputs number less than 8 or more than 128 then an alert to try again appears and then directed to input again
+      if (lengthPrompt < 8 || lengthPrompt > 128) {
+        window.alert("That wasn't between 8 and 128 characters, please try again.")
+        firstPrompt()
+      } 
 
 //If the user inputs a number between 8 and 128 then that number is returned in lengthPrompt
-    else {
-      return lengthPrompt;
-    }
-  }; 
+      else {
+        return lengthPrompt;
+      };
+
+//If the user decided to cancel at the length prompt then they are alerted with a goodbye message and the project stops functioning
+    } else {
+      window.alert("Cheerio.")
+      return;
+    }; 
+  }
+
   
 //secondPrompt for types of characters//
 //JUST INPUT THE CONFIRM PROMPTS FOR DIFFERENT SYMBOLS//
