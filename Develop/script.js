8cae92798e7a2db7dcdf3086237274b33fcbff3d
Ben Smerd <bsmerd@adam.com.au>
2022-06-24 17:49:08 +0930

    Changed around the layout and modified the firstPrompt as well as adding in character selector strings

--- Develop/script.js
+++ Develop/script.js
@@ -9,35 +9,62 @@ function writePassword() {
   
 }
 
+let lengthPrompt;
+let charsLowString = "abcdefghijklmnopqrstuvwxyz";
+let charsUpString = charsLowString.toLocaleUpperCase();
+let charsSymbString = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+let charsNumString = "0123456789"
+
+/*Function to turn the strings of characters into arrays*/
+
+//function to generate the password//
 function generatePassword() {
-
   //asking user if they want to begin and with criteria//
-  var confirm = window.confirm("The password will contain a series of numbers that you get to select as well as the length, are you ready to begin?");
+  let confirm = window.confirm("The password will contain a series of numbers that you get to select as well as the length, are you ready to begin?");
 
-  let lengthPrompt;
-  //if the user wants to begin or not creating the password criteria with function//
-  //if they confirm they want to being then first prompt asking about length of characters//
-  var firstPrompt = function() {
-    if (confirm) {
-    lengthPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?");
-    return lengthPrompt;
-  } else {
+  //If user confirms to continue, firstPrompt is initialized
+  if (confirm) {
+    firstPrompt(confirm);
+  } 
+
+  //If user denies to continue then they are told goodbye and program ended
+  else {
     window.alert("Cheerio.")
     return;
-  }
 }
-  
-  firstPrompt()
 
-  if (lengthPrompt < 8 || lengthPrompt > 128) {
-    window.alert("That wasn't between 8 and 128 characters, please try again.")
-    firstPrompt()
+
+
+};
+
+//if the user wants to begin or not creating the password criteria with function//
+//if they confirm they want to being then first prompt asking about length of characters//
+  var firstPrompt = function() {
+//Prompt window asking for how many characters between 8 and 128
+    lengthPrompt = window.prompt("Let's begin with the length, how many characters do you want between 8 and 128 characters?");
+
+//If the user inputs number less than 8 or more than 128 then an alert to try again appears
+    if (lengthPrompt < 8 || lengthPrompt > 128) {
+      window.alert("That wasn't between 8 and 128 characters, please try again.")
+      firstPrompt()
+    } 
+
+//If the user inputs a number between 8 and 128 then that number is returned in lengthPrompt
+    else {
+      return lengthPrompt;
+    }
+  }; 
+  
+//secondPrompt for types of characters//
+//JUST INPUT THE CONFIRM PROMPTS FOR DIFFERENT SYMBOLS//
+  var secondPrompt = function() {
+    
   }
 
 
 
 
-} 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
